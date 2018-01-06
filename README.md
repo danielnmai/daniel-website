24da5fa1b10bc6bcea6e879780ce3af7dfc08508
Daniel Mai <danielmai1728@gmail.com>
2018-01-05 22:49:02 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+My Personal Website Under Construction
+
+It is built purely on React using Create React App tool.
+
+Project started on January 5, 2017.
+Copyright @ DANIEL MAI
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Button } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+         <h3> Daniel Website Under Construction </h3>
+      </div>
+    );
+  }
+}
+
+export default App;
